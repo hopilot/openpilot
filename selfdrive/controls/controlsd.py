96d72e5440f9d1853ae398ef24a9e527bba933c9
hikee9123 <hikee9123@gmail.com>
2022-06-26 15:54:47 +0900

    fix

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -355,20 +355,16 @@ class Controls:
     elif not self.sm.valid["pandaState"]:
       self.events.add(EventName.usbError)
     elif not self.sm.all_checks() or self.can_rcv_error:
-      log_comm_issue = False
       if self.commIssue_ignored or self.map_enabled:
         pass
       elif not self.sm.all_alive():
-        log_comm_issue = True
         self.events.add(EventName.commIssue)
       elif not self.sm.all_freq_ok():
-        log_comm_issue = True
         self.events.add(EventName.commIssueAvgFreq)
       else: # invalid or can_rcv_error.
-        log_comm_issue = True
         self.events.add(EventName.commIssue)
 
-      if log_comm_issue and not self.logged_comm_issue:
+      if not self.logged_comm_issue:
         invalid = [s for s, valid in self.sm.valid.items() if not valid]
         not_alive = [s for s, alive in self.sm.alive.items() if not alive]
         cloudlog.event("commIssue", invalid=invalid, not_alive=not_alive, can_error=self.can_rcv_error, error=True)
