1c56c086b3705401135cebda15c97bd23349fd9c
Chanho Jung <jungch@naver.com>
2022-12-08 22:33:48 +0900

    LatControl: remove duplicate logic for saturation (#26725)

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -753,7 +753,7 @@ class Controls:
       max_torque = abs(self.last_actuators.steer) > 0.99
       if undershooting and turning and good_speed and max_torque:
         self.events.add(EventName.steerSaturated)    
-    elif lac_log.active and lac_log.saturated and not CS.steeringPressed:
+    elif lac_log.active and not CS.steeringPressed:
       dpath_points = lat_plan.dPathPoints
       if len(dpath_points):
         # Check if we deviated from the path
